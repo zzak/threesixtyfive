2ccbde38f44ff6325c2045f2c894efa5d6870db4
Zachary Scott <zachary.s.scott@gmail.com>
2010-07-31 19:08:48 -0400

    adding devise to threesixtyfive for authorative purposes http://github.com/plataformatec/devise :)

---
+++ app/models/user.rb
@@ -0,0 +1,8 @@
+class User < ActiveRecord::Base
+  # Include default devise modules. Others available are:
+  # :token_authenticatable, :lockable, :registerable, :timeoutable, :confirmable and :activatable
+  devise :database_authenticatable, :recoverable, :rememberable, :trackable, :validatable
+
+  # Setup accessible (or protected) attributes for your model
+  attr_accessible :email, :password, :password_confirmation
+end
